0adfd826e33b86a75155eb4532df32dae0177ca9
vedants27 <vedantss27@gmail.com>
2025-03-05 23:52:13 +0530

    Added menu  for user interaction.

--- Main.java
+++ Main.java
@@ -10,6 +10,25 @@ class Main {
         Scanner scanner = new Scanner(System.in); // Scanner to take input
         Deck deck = new Deck(); // Initialize a new Deck
         int choice;
+
+        // Display Menu to User
+        do {
+        System.out.println("\n===== Card Deck Menu =====");
+        System.out.println("1. Create Deck");
+        System.out.println("2. Print Deck");
+        System.out.println("3. Print a Specific Card");
+        System.out.println("4. Find Cards of Same Suit");
+        System.out.println("5. Compare Two Cards");
+        System.out.println("6. Search for a Card");
+        System.out.println("7. Deal 5 Random Cards");
+        System.out.println("8. Shuffle Deck");
+        System.out.println("0. Exit");
+        System.out.print("Enter your choice: ");
+
+        choice = scanner.nextInt(); // Take integer input for user choice
+        scanner.nextLine(); // Consume newline character
+
+
     }
 }
 
