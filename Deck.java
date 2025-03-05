ebc4ff3e42213e39e6e9ac2e000fb4dd5dbda87a
vedants27 <vedantss27@gmail.com>
2025-03-06 00:00:57 +0530

    Added Deck class and constructor.

--- Deck.java
+++ Deck.java
@@ -1,1 +1,13 @@
+// Deck.java 
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Deck {
+    ArrayList<Card> deck;
+
+    public Deck() {
+        this.deck = new ArrayList<>();
+    }
+}
 
